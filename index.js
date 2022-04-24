0455e74e3db2f1950db9636ebff78151db59ca71
Luis <luis.mogueadp@gmail.com>
2022-04-24 11:41:19 -0500

    Se agrega proyecto

---
+++ index.js
@@ -0,0 +1,10 @@
+function validateUserAge(age) {
+    if (age === 23){
+        return "Able to adquire credit"
+    }
+    else {
+        return "Not able to adquire credit"
+    }
+}
+
+console.log(validateUserAge("22"))
